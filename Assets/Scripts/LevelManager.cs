863ab15168fd782f518b81a04e02fc04a7050b41
BenjaminLM95 <benjamin.lerner95@gmail.com>
2025-01-30 14:55:47 -0400

    Loading Scene Fixed

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
     public GameObject player;
-    private GameObject SpawnPosition; 
+    public GameObject SpawnPosition;
     // Start is called before the first frame update
     void Start()
     {      
@@ -17,9 +18,8 @@ public class LevelManager : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(KeyCode.Alpha2)) 
-        {
+        {            
             LoadGamePlayScene();
-
         }
 
         if (Input.GetKey(KeyCode.Alpha1))
@@ -32,24 +32,43 @@ public class LevelManager : MonoBehaviour
     void LoadGamePlayScene() 
     {
         SceneManager.LoadScene(1);
-        //SpawnPosition = GameObject.Find("SpawnPointGamePlay");
-        //player.transform.position = SpawnPosition.transform.position;
-
-        SceneManager.sceneLoaded += onSceneLoaded;
+        SceneManager.sceneLoaded += onSceneLoaded;     
+                        
     }
 
     void LoadMainMenuScene()
     {
         SceneManager.LoadScene(0);
-
-        SceneManager.sceneLoaded += onSceneLoaded; 
-        //SpawnPosition = GameObject.Find("SpawnPointMainMenu");
-        //player.transform.position = SpawnPosition.transform.position;
+        SceneManager.sceneLoaded += onSceneLoaded;     
+               
     }
 
     void onSceneLoaded(Scene scene, LoadSceneMode mode) 
     {
-        Debug.Log("Scene: " + scene.name + "is loaded"); 
+        Debug.Log("Scene: " + scene.name + "is loaded");
+
+        if (scene.name == "GamePlay")
+        {
+            SpawnPosition = GameObject.Find("SpawnPointGamePlay");
+            player.transform.position = SpawnPosition.transform.position;
+        }
+
+        else if (scene.name == "MainMenu")
+        {
+            SpawnPosition = GameObject.Find("SpawnPointMainMenu");
+            player.transform.position = SpawnPosition.transform.position;
+        }
+        else
+        {
+
+        }
+        Debug.Log(SpawnPosition.name); 
+    }   
+    
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= onSceneLoaded;
     }
+    
 
 }
